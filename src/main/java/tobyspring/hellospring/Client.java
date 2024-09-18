76e88398e796fed29962b17a6d4e42d14bc923b2
NaMinhyeok <nmh9097@gmail.com>
2024-09-18 18:21:51 +0900

    section3.3 실습예제 연습
    - 인터페이스 도입, 관계 설정 책임의 분리
    - 인터페이스를 통해서 재사용하기 쉽게 만듦
    - PaymentService에서 Provider를 고르는게 아닌 Client가 고르도록 바꾸어서 관계설정의 책임을 분리한다

--- src/main/java/tobyspring/hellospring/Client.java
+++ src/main/java/tobyspring/hellospring/Client.java
@@ -1,11 +1,10 @@
 package tobyspring.hellospring;
 
-import java.io.IOException;
 import java.math.BigDecimal;
 
 public class Client {
-    public static void main(String[] args) throws IOException {
-        PaymentService paymentService = new SimpleExRatePaymentService();
+    public static void main(String[] args) throws Exception {
+        PaymentService paymentService = new PaymentService(new WebApiExRateProvider());
         Payment payment = paymentService.prepare(1L,"USD", BigDecimal.valueOf(13.8));
         System.out.println(payment);
     }
--- src/main/java/tobyspring/hellospring/SimpleExRateProvider.java
+++ src/main/java/tobyspring/hellospring/SimpleExRateProvider.java
@@ -3,10 +3,11 @@ package tobyspring.hellospring;
 import java.io.IOException;
 import java.math.BigDecimal;
 
-public class SimpleExRatePaymentService extends PaymentService{
+public class SimpleExRateProvider implements ExRateProvider {
+
     @Override
-    BigDecimal getExRate(String currency) throws IOException {
-        if(currency.equals("USD")) {
+    public BigDecimal getExRate(String currency) throws IOException{
+        if (currency.equals("USD")) {
             return BigDecimal.valueOf(1000);
         } else {
             throw new IllegalArgumentException(currency + "는 지원하지 않는 통화입니다 !");
