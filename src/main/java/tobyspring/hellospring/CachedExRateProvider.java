b7b569d68d384b6ffeb4389751a84c9410527a48
NaMinhyeok <nmh9097@gmail.com>
2024-09-20 17:41:13 +0900

    section3 실습예제 DI, Decorator 패턴

---
+++ src/main/java/tobyspring/hellospring/CachedExRateProvider.java
@@ -0,0 +1,29 @@
+package tobyspring.hellospring;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class CachedExRateProvider implements ExRateProvider {
+
+    private final ExRateProvider target;
+
+    private BigDecimal cachedExRate;
+    private LocalDateTime cacheExpireTime;
+
+    public CachedExRateProvider(ExRateProvider target) {
+        this.target = target;
+    }
+
+    @Override
+    public BigDecimal getExRate(String currency) throws IOException {
+        if (cachedExRate == null || cacheExpireTime.isBefore(LocalDateTime.now())) {
+            cachedExRate = this.target.getExRate(currency);
+            cacheExpireTime = LocalDateTime.now().plusSeconds(5);
+
+            System.out.println("Cache Updated");
+        }
+
+        return cachedExRate;
+    }
+}
--- src/main/java/tobyspring/hellospring/Client.java
+++ src/main/java/tobyspring/hellospring/Client.java
@@ -4,13 +4,26 @@ import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.math.BigDecimal;
+import java.util.concurrent.TimeUnit;
 
 public class Client {
     public static void main(String[] args) throws Exception {
         BeanFactory beanFactory = new AnnotationConfigApplicationContext(ObjectFactory.class);
         PaymentService paymentService = beanFactory.getBean(PaymentService.class);
 
-        Payment payment = paymentService.prepare(1L, "USD", BigDecimal.valueOf(13.8));
-        System.out.println(payment);
+        Payment payment1 = paymentService.prepare(1L, "USD", BigDecimal.valueOf(13.8));
+        System.out.println("Payment1 : " + payment1);
+
+        System.out.println("-------------------------------------------------------");
+        TimeUnit.SECONDS.sleep(3);
+
+        Payment payment2 = paymentService.prepare(1L, "USD", BigDecimal.valueOf(13.8));
+        System.out.println("Payment2 : " + payment2);
+        System.out.println("-------------------------------------------------------");
+
+        TimeUnit.SECONDS.sleep(5);
+
+        Payment payment3 = paymentService.prepare(1L, "USD", BigDecimal.valueOf(13.8));
+        System.out.println("Payment3 : " + payment3);
     }
 }
